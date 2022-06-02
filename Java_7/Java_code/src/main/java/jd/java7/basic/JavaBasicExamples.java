b2eb79e3dc41b262dca14d97566bcfecf8c11125
JDBIRLA <jitendra.birla21@gmail.com>
2022-06-02 07:06:53 +0530

    java7 prep

---
+++ Java_7/Java_code/src/main/java/jd/java7/basic/JavaBasicExamples.java
@@ -0,0 +1,14 @@
+package jd.java7.basic;
+
+public class JavaBasicExamples {
+	
+
+	public static void main(String[] args) {
+		 final  int i=0;
+		// TODO Auto-generated method stub
+     System.out.println("Hello Prabu, Gurudev, ....Please give me success for my hardwork");
+	 System.out.println(i+1);
+	
+	}
+
+}
