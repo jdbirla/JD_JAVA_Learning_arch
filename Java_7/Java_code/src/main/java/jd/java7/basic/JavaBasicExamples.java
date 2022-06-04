d050d6af3ae22d82191ed6cc8c319760b848044d
JDBIRLA <jitendra.birla21@gmail.com>
2022-06-04 12:22:10 +0530

    java7 prep

--- Java_7/Java_code/src/main/java/jd/java7/basic/JavaBasicExamples.java
+++ Java_7/Java_code/src/main/java/jd/java7/basic/JavaBasicExamples.java
@@ -7,8 +7,19 @@ public class JavaBasicExamples {
 		 final  int i=0;
 		// TODO Auto-generated method stub
      System.out.println("Hello Prabu, Gurudev, ....Please give me success for my hardwork");
-	 System.out.println(i+1);
-	
+	 
+     String s1 = new String("10");
+     String s2 = new String("soft");
+     String s3 = new String("1");
+     String s4=s1;
+   
+     System.out.println(s1.hashCode());
+     System.out.println(s2.hashCode());
+     System.out.println(s3.hashCode());
+
+     System.out.println(s1==s2);
+     System.out.println(s1==s4);
+
 	}
 
 }
