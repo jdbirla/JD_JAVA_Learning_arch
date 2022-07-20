1ac8e4683fa6da41d109c597351ed66b38096b9c
Jitendra Birla <jbirla@dxc.com>
2022-07-20 10:49:14 +0900

    java8 code

--- Java_8/Java_8_In_Action/Java8InAction-master/src/main/java/lambdaasinaction/jd/JavaCodingInterview.java
+++ Java_8/Java_8_In_Action/Java8InAction-master/src/main/java/lambdaasinaction/jd/JavaCodingInterview.java
@@ -323,6 +323,7 @@ public class JavaCodingInterview {
 				
 
 
+}
 };
 
 interface MultiplyInterface {
@@ -469,17 +470,5 @@ class EmployeeDatabase {
 				
 				).collect(Collectors.toList());
 	}
-}
+};
 
-<<<<<<< HEAD
-=======
-        public static List<Employee> getEmployees(){
-          return  Stream.of(new Employee(101,"john","A","DEV",60000),
-                  new Employee(109,"peter","B","DEV",30000),
-                  new Employee(102,"mak","A","QA",80000),
-                  new Employee(103,"kim","A","QA",90000),
-                  new Employee(104,"json","C","DEVOPS",15000))
-                  .collect(Collectors.toList());
-        }
-    }
->>>>>>> faf39a9547d06028e460aae91310a56accef5230
